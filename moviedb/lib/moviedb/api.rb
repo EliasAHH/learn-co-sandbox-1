76402a90c458a18f12fd2b3c5c80eb5bd78331dd
Tammy Lam <lamtammy1515@gmail.com>
2020-03-07 00:50:37 +0000

    Automatically backed up by Learn

--- moviedb/lib/moviedb/api.rb
+++ moviedb/lib/moviedb/api.rb
@@ -2,7 +2,8 @@ class Moviedb::API
   
   def fetch(tvshow)
     key = "2ca8c5ce77b6ae56746eebc245c26e0a"
-    url = 
+    url = "https://api.themoviedb.org/3/search/movie?api_key=#{key}&query=#{tvshow}"
+    response = HTTParty
     
   
 end 
