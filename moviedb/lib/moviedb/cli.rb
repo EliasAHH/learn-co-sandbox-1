904bb27c65375b47274766e443f700634dc3631a
Tammy Lam <lamtammy1515@gmail.com>
2020-03-07 00:42:08 +0000

    Automatically backed up by Learn

--- moviedb/lib/moviedb/cli.rb
+++ moviedb/lib/moviedb/cli.rb
@@ -4,6 +4,29 @@ class Moviedb::CLI
     start 
   end 
   
+  def start
+    input = ""
+    unless input == 'exit'
+      list_options
+      input = gets.strip.downcase
+      @api = API.new(input)
+      if input == 'exit'
+        exit_message
+      elsif input == 'all' || input.to_i.between?(1890, 2019)
+        @api.fetch
+        list_movies(input)
+        menu
+      else
+        wrong_input
+        start
+      end
+    end
+  end
   
+  def list_options
+    puts "Enter a year as an integer to see the best-selling films of that year."
+    puts "Enter 'all' to see the best-selling films of all time."
+    puts "Enter 'exit' to end the program."
+  end
   
 end 
